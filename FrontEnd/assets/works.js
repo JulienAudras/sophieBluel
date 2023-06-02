bede735da9cd7f49e432eb8d3c40755225629451
Julien Audras <audras.julien@gmail.com>
2023-06-02 15:13:40 +0200

    Filtres avec selection

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -1,4 +1,64 @@
+// // // Creating Filters // // //
+const filtersArray =[];
+async function generateFilters(){
+
+    const filterSection = document.querySelector(".filters");
+    
+    const filter = document.createElement("div");
+    filter.classList.add("filter");
+    filter.setAttribute("id", "selected")
+    filter.innerText = "Tous";
+    filterSection.appendChild(filter);
+    filtersArray.push(filter); 
+   
+    await fetch('http://localhost:5678/api/works')
+    .then(response => response.json())
+    .then(data => {
+        const categoriesSet = new Set();
+
+        data.forEach(item => {
+            const { name } = item.category;
+            categoriesSet.add(name);
+        })
+        
+        const categories = Array.from(categoriesSet);
+        
+        categories.forEach(category => {
+            const filter = document.createElement("div");
+            filter.classList.add("filter")
+            filter.innerText = category;
+            filterSection.appendChild(filter);
+            filtersArray.push(filter);
+        })
+    })
+}
+
+generateFilters()
+
+
+// // // Filter Function // // //
+const filterSection = document.querySelector(".filters");
+
+function update(index) {
+    
+  for (let i = 0; i < filtersArray.length; i++) {
+    if (i === index) {
+      filtersArray[i].setAttribute("id", "selected");
+    } else {
+      filtersArray[i].removeAttribute("id");
+    }
+  }
+}
+
+filterSection.addEventListener("click", function(event) {
+  const selectedFilter = event.target;
+  const index = filtersArray.indexOf(selectedFilter);
+  update(index);
+});
+    
+
 // // // Extract datas from api and create elements on page // // //
+
 async function getWorksDatas(){
 
     await fetch('http://localhost:5678/api/works')
@@ -18,7 +78,6 @@ async function getWorksDatas(){
             gallery.appendChild(workElement);
             workElement.appendChild(imgWorkElement);
             workElement.appendChild(captionWorkElement);
-
         });
     })
 }
